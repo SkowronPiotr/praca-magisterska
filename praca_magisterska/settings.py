eb77c27669a4a9d22b66a8373e7977317b641b2b
Piotr Skowron <skowronpiotrmail@gmail.com>
2024-04-05 18:04:02 +0200

    empty commit

--- praca_magisterska/settings.py
+++ praca_magisterska/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%&*9g3-a32q4j&$gabxqux7yclv16bks4-(b#2s0qtq6faa4c#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
